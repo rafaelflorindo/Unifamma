52989f3d07380c061ef8af9a00dd52f425c786a8
rafaelflorindo <rafaelflorindo@hotmail.com>
2020-04-24 20:49:33 -0300

    Add files via upload

--- DesenvolvimentoWeb/Aula24-04-2020/controller/logar.php
+++ DesenvolvimentoWeb/Aula24-04-2020/controller/logar.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 if (isset($_POST)){
     if (!empty($_POST["usuario"]) && !empty($_POST["senha"])){
         $usuario = filter_input(INPUT_POST, "usuario", FILTER_SANITIZE_STRING);
@@ -9,7 +10,8 @@ if (isset($_POST)){
         $autenticarUsuario = new Autenticar();
         $logon = $autenticarUsuario->autenticarUsuario($usuario, $senha);
         if ($logon){
-            $_SESSION["nome"] = $logon[0]["nome"];
+            $_SESSION["nomeUsuario"] = $logon[0]["nome"];
+            $_SESSION["emailUsuario"] = $logon[0]["email"];
             header('location: ../index.php');
         }else{
           header('location: ../view/formularioLogin.php?mensagem=erroUsuario');
